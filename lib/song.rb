6e38f336963395f5299df99a71f3854d9d922683
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-22 10:40:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ attr_accessor = :name, :artist, :genre
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
-    @genre = genre
+    @genre = genre 
     @@count += 1 
     @@genres << genre 
     @@artists << artist
